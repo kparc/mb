383856cda4c63e20185df85176e9aa469e69a5e0
kelas <me@kel.as>
2024-02-01 07:12:30 +0100

    [~] arena

--- c.h
+++ c.h
@@ -4,11 +4,13 @@
 
 typedef double F;
 
+#define ARENA 100000
+
 #define BENCH()           F wall=0;struct timespec start,end;
 #define TIME(x)           clock_gettime(CLOCK_REALTIME,&x);
 #define MSEC(x)           (x.tv_sec*1000+x.tv_nsec/1000000.0)
 #define WALL(a...)        TIME(start);a;TIME(end);wall=(MSEC(end)-MSEC(start))/1000.0;
-#define SEED              I*arena=calloc(4,100000),n=0;
+#define SEED              I*arena=malloc(4*ARENA),n=0;
 //#define SEED              srand(time(0));I*arena=calloc(4,100000),n=0;
 
 #define ROUNDS 200
@@ -23,11 +25,8 @@ int pick(){
 }
 
 int pick2(){
-    return (rand() % (10000000 - 0 + 1)) + 0;
+    return (rand() % (ARENA - 0 + 1)) + 0;
 }
-
-const size_t bigger_than_cachesize = 10 * 1024 * 1024;
-long p[bigger_than_cachesize];
 
 //#define BUSYWORK() for(int i=0;i<bigger_than_cachesize;i++)p[i]=rand();
 #define BUSYWORK()  N(10000000,n+=arena[pick2()])
--- makefile
+++ makefile
@@ -1,6 +1,6 @@
-CC=gcc
+CC=clang
 all:
 	rm -f m && $(CC) -ffast-math -fno-unwind-tables -fno-stack-protector -O3 m.c -o m
-	./m
+	time ./m
 	rm -f l && $(CC) -ffast-math -fno-unwind-tables -fno-stack-protector -O3 l.c -o l
-	./l
+	time ./l

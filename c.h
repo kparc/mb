47efb8e5b9f1871fdadf88e7d0b757c29f7ae44e
kelas <me@kel.as>
2024-02-01 07:20:42 +0100

    [~]

--- c.h
+++ c.h
@@ -10,7 +10,7 @@ typedef double F;
 #define TIME(x)           clock_gettime(CLOCK_REALTIME,&x);
 #define MSEC(x)           (x.tv_sec*1000+x.tv_nsec/1000000.0)
 #define WALL(a...)        TIME(start);a;TIME(end);wall=(MSEC(end)-MSEC(start))/1000.0;
-#define SEED              I*arena=malloc(4*ARENA),n=0;
+#define SEED              I*arena=malloc(4*ARENA),n=0;N(ARENA,arena[i]=i);
 //#define SEED              srand(time(0));I*arena=calloc(4,100000),n=0;
 
 #define ROUNDS 200
